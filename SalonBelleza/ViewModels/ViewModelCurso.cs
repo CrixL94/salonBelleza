9a098f549d79ac8c54d033f03801b1aaa019b3a5
Cristhian <96554800+CrixL94@users.noreply.github.com>
2022-12-05 20:28:15 -0600

    commit curso
    se agrego vista y se creo ViewModelCurso

---
+++ SalonBelleza/ViewModels/ViewModelCurso.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace SalonBelleza.ViewModels
+{
+    public class ViewModelCurso : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
